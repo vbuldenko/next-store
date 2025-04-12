e3ca60182a65851f0d032b01f54a667634f6589f
Vladyslav <vladbuldenko@gmail.com>
2025-04-12 19:02:02 +0300

    Small reorganization and Product components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "@/components/shared/Footer";
-import Header from "@/components/shared/Header";
-import HeaderCategorySelector from "@/components/shared/Header/CategorySelector";
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+import HeaderCategorySelector from "@/components/layout/header/CategorySelector";
 
 export default function RootLayout({
   children,
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
+import ProductList from "@/components/product/ProductList";
+import sampleData from "@/lib/db/sample-data";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </main>
+  );
 }
