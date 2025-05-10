9f86d9388e34d0378a473fdb82267f4a83a77376
Vladyslav <vladbuldenko@gmail.com>
2025-05-10 20:12:11 +0300

    Next config for images loaded from outer sources

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["lh3.googleusercontent.com"], //Google's domain for profile pictures
+  },
 };
 
 export default nextConfig;
