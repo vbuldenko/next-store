b2850663d5dc40eeeecdfb0f9ef7985d6304cbf9
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2025-05-13 11:12:11 +0300

    Minor adjustments

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,17 @@
-import { UserDetails } from "../../components/UserDetails";
+// import { UserDetails } from "../../components/UserDetails";
 
 export default async function DashboardPage() {
   return (
     <main className="max-w-[75rem] w-full mx-auto">
       <div className="grid grid-cols-[1fr_20.5rem] gap-10 pb-10">
-        <UserDetails />
+        {/* <UserDetails /> */}
+        <div className="flex flex-col gap-5">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <p className="text-sm text-gray-500">
+            Welcome to your dashboard. Here you can manage your account and
+            settings.
+          </p>
+        </div>
       </div>
     </main>
   );
