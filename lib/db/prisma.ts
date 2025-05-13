eb4706873efc707d87ebe11fee3d36c19ae78bab
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2025-05-13 10:54:44 +0300

    Prisma config via neon adapter to work with neon db

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,11 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "../../generated/prisma";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaNeon({ connectionString });
+export const prisma = new PrismaClient({ adapter });
