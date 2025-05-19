4f8fabf37e990f1a129a7b8529bb9fb5b7cd06e9
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2025-05-19 15:11:22 +0300

    Button variants adjustments

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,15 +3,15 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { clsx } from "clsx";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "cursor-pointer inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-black text-white hover:bg-blue-600 cursor-pointer",
+        default: "bg-black text-white hover:bg-blue-600",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "rounded-[6px] border-0 bg-[linear-gradient(156deg,_#fff,_#f0f0f0)] shadow hover:bg-gray-100 hover: text-gray-900",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
