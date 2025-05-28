b0da8f8c23834c8b3b0f10b8138b6f5d4575d40b
Vladyslav <vladbuldenko@gmail.com>
2025-05-29 00:04:44 +0300

    PlaceOrdeForm update

--- components/order/PlaceOrderForm.tsx
+++ components/order/PlaceOrderForm.tsx
@@ -1,39 +1,51 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useFormStatus } from "react-dom";
+import { useTransition } from "react";
 import { createOrder } from "@/lib/actions/order.actions";
-
+import { toast } from "sonner";
 import { IoCheckmarkDone } from "react-icons/io5";
 import ProceedButton from "../ui/proceedButton";
 
 const PlaceOrderForm = () => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    const res = await createOrder();
+    startTransition(async () => {
+      try {
+        const res = await createOrder();
 
-    if (res.redirectTo) {
-      router.push(res.redirectTo);
-    }
-  };
+        if (!res.success) {
+          toast.error(res.message);
+          if (res.redirectTo) {
+            router.push(res.redirectTo);
+          }
+          return;
+        }
 
-  const PlaceOrderButton = () => {
-    const { pending } = useFormStatus();
-    return (
-      <ProceedButton
-        isLoading={pending}
-        text="Place Order"
-        icon={<IoCheckmarkDone className="size-5" />}
-      />
-    );
+        toast.success(res.message);
+        if (res.redirectTo) {
+          router.push(res.redirectTo);
+        }
+      } catch (error) {
+        console.error("Order creation error:", error);
+        toast.error("Something went wrong. Please try again.");
+      }
+    });
   };
 
   return (
     <form onSubmit={handleSubmit} className="w-full">
-      <PlaceOrderButton />
+      <ProceedButton
+        isLoading={isPending}
+        text="Place Order"
+        icon={<IoCheckmarkDone className="size-5" />}
+        type="submit"
+        className="w-full"
+      />
     </form>
   );
 };
